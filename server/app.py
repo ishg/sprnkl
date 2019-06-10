13bc63040c95eaf9e11348fb63bfbfa78718e5ce
Ishmeet Grewal <ishmeetsinghgrewal@gmail.com>
2019-06-10 00:54:22 -0700

    Updating UI for schedules

--- server/app.py
+++ server/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
-import datetime
-import RPi.GPIO as GPIO
+import flask
+import datetime, json
+# import RPi.GPIO as GPIO
 
-app = Flask(__name__)
-GPIO.setmode(GPIO.BCM)
+app = flask.Flask(__name__)
+# GPIO.setmode(GPIO.BCM)
 
+'''
 zones = [
     {"pin": 11, "name": "zone 1-4 (back tree)", "status": 0},
     {"pin": 9, "name": "zone 2-6 (back garden)", "status": 0},
@@ -17,32 +18,43 @@ zones = [
     {"pin": 15, "name": "zone 11 (front kyaaris)", "status": 0},
     {"pin": 3,"name": "zone 12 (front kyaaris)", "status": 0},
 ]
+'''
 
+'''
 for zone in zones:
     GPIO.setup(zone['pin'], GPIO.OUT)
     GPIO.output(zone['pin'], GPIO.LOW)
+'''
+
+data = {}
+
+@app.before_request
+def getData():
+    with open('config.json') as json_file:
+        flask.g.data = json.load(json_file)
+        print flask.g.data["schedules"]
+
 
 @app.route("/")
 def main():
-    for zone in zones:
-	zone["status"] = GPIO.input(zone["pin"])
-    #print zones
-    templateData = { 'zones': zones }
-    return render_template('main.html', **templateData)
+
+    #for zone in zones:
+	#   zone["status"] = GPIO.input(zone["pin"])
+    templateData = { 'zones': flask.g.data['zones'], 'schedules': flask.g.data["schedules"] }
+    return flask.render_template('main.html', **templateData)
 
 @app.route("/<changePin>/<action>")
 def action(changePin, action):
     changePin = int(changePin)
 
-    if action == "on":
-        GPIO.output(changePin, GPIO.HIGH)
-    if action == "off":
-        GPIO.output(changePin, GPIO.LOW)
-    for zone in zones:
-	zone["status"] = GPIO.input(zone["pin"])
-    #print zones
-    templateData = {'zones': zones}
-    return render_template('main.html', **templateData)
+    #if action == "on":
+    #    GPIO.output(changePin, GPIO.HIGH)
+    #if action == "off":
+    #    GPIO.output(changePin, GPIO.LOW)
+    #for zone in zones:
+	#  zone["status"] = GPIO.input(zone["pin"])
+    templateData = { 'zones': flask.g.data['zones'], 'schedules': flask.g.data["schedules"] }
+    return flask.render_template('main.html', **templateData)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=5000)
