3808b2adf4540324cf9c3105257ad294b4a86ccb
Ishmeet Grewal <ishmeetsinghghrewal@gmail.com>
2019-06-10 22:36:35 -0700

    Removing Expo App

--- app-expo/components/StyledText.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Text } from 'react-native'
-
-export default class StyledText extends React.Component {
-  render () {
-    return <Text {...this.props} style={[{ fontFamily: 'rubik' }, this.props.style]} />
-  }
-}
