d3eb2e6a5fc79d3fe239d2acebacc417861c827d
Ishmeet Grewal <ishmeetsinghghrewal@gmail.com>
2019-06-11 00:05:40 -0700

    Sending UI to Pi

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <dashboard>
-
     <div id="new_modal" class="modal bottom-sheet">
       <div class="modal-content">
         <h4>New Schedule</h4>
@@ -28,9 +27,9 @@
         <div class="section">
           <span>Watering Start Time</span>
           <span class="right">
-            <input style="max-width: 100px" type="text" class="timepicker" v-model.lazy="new_schedule.time" id="new_schedule_time" >
-            <i class="material-icons right" style="margin-left: 0px; margin-right: -5px; color: color: #50AE54;">chevron_right</i>
-          </span>
+                        <input style="max-width: 100px" type="text" class="timepicker" v-model.lazy="new_schedule.time" id="new_schedule_time">
+                        <i class="material-icons right" style="margin-left: 0px; margin-right: -5px; color: color: #50AE54;">chevron_right</i>
+                    </span>
         </div>
         <div class="section">
           <span>Watering Duration</span>
@@ -43,30 +42,25 @@
         <a href="#!" v-on:click="createSchedule()" class="waves-effect waves-green btn green">Save</a>
       </div>
     </div>
-
     <div class="row">
       <div class="col s12">
         <div class="card">
           <ul class="collection with-header">
-            <li class="collection-header"><h4>Hobstone Sprinklers</h4></li>
+            <li class="collection-header">
+              <h4>Hobstone Sprinklers</h4>
+            </li>
             <li v-for="(zone, index) in zones" :key="index" class="collection-item">
-            <div>{{zone.name}}
-
-            <div class="switch right">
-              <label>
-                Off
-                <input v-model="zone.status" type="checkbox" v-on:change="updateZone(zone)">
-                <span class="lever"></span>
-                On
-              </label>
-            </div>
-
-            <!-- <a v-show="zone.status" :href="'/' + zone.pin + '/off'" class="secondary-content">
-            <i class="material-icons red-text">power_off</i>
-            </a>
-            <a v-show="!zone.status" :href="'/'+zone.pin+ '/on'" class="secondary-content">
-            <i class="material-icons green-text">power_settings_new</i></a> -->
-            </div></li>
+              <div>{{zone.name}}
+                <div class="switch right">
+                  <label>
+                    Off
+                    <input v-model="zone.status" type="checkbox" v-on:change="updateZone(zone)">
+                    <span class="lever"></span>
+                    On
+                  </label>
+                </div>
+              </div>
+            </li>
           </ul>
         </div>
       </div>
@@ -74,73 +68,71 @@
     <div class="row">
       <div class="col s12">
         <div class="card">
-            <ul class="collapsible collection with-header">
-              <li class="collection-header" style="line-height: 35px;">
-                <span style="font-size: 30px;">Schedules</span>
-                <a href="#new_modal" class="btn green secondary-content modal-trigger"><i class="material-icons left">add</i>New</a>
-              </li>
-              <li v-for="(s, index) in schedules" :key="index">
-                <div class="collapsible-header">
-                  <i v-show="s.enabled" class="material-icons green-text">play_circle_outline</i>
-                  <i v-show="!s.enabled" class="material-icons red-text">pause_circle_outline</i>
-                  {{ s.name }}
-
+          <ul class="collapsible collection with-header">
+            <li class="collection-header" style="line-height: 35px;">
+              <span style="font-size: 30px;">Schedules</span>
+              <a href="#new_modal" class="btn green secondary-content modal-trigger"><i class="material-icons left">add</i>New</a>
+            </li>
+            <li v-for="(s, index) in schedules" :key="index">
+              <div class="collapsible-header">
+                <i v-show="s.enabled" class="material-icons green-text">play_circle_outline</i>
+                <i v-show="!s.enabled" class="material-icons red-text">pause_circle_outline</i>
+                {{ s.name }}
+              </div>
+              <!--- NEW SECTION --->
+              <div class="collapsible-body green lighten-5">
+                <div class="section">
+                  <span>Enable Schedule</span>
+                  <div class="switch right">
+                    <label>
+                      Off
+                      <input v-model="s.enabled" type="checkbox">
+                      <span class="lever"></span>
+                      On
+                    </label>
+                  </div>
                 </div>
-                <!--- NEW SECTION --->
-                <div class="collapsible-body green lighten-5">
+                <div v-show="s.enabled">
                   <div class="section">
-                    <span>Enable Schedule</span>
-                    <div class="switch right">
-                      <label>
-                        Off
-                        <input v-model="s.enabled" type="checkbox">
-                        <span class="lever"></span>
-                        On
+                    <span>Schedule Zones</span>
+                    <p v-for="(z, idx) in zones" :key="idx">
+                      <label style="color: black">
+                        <input type="checkbox" :id="'schedule_' + s.id + '_zone_' + z.id" class="filled-in" :checked="s.zones.includes(z.id)" v-on:change="dirty(s)" />
+                        <span>{{z.name}}</span>
                       </label>
-                    </div>
+                    </p>
                   </div>
-                  <div v-show="s.enabled">
-                    <div class="section">
-                      <span>Schedule Zones</span>
-                      <p v-for="(z, idx) in zones" :key="idx">
-                        <label style="color: black">
-                          <input type="checkbox" :id="'schedule_' + s.id + '_zone_' + z.id" class="filled-in" :checked="s.zones.includes(z.id)" v-on:change="dirty(s)" />
-                          <span>{{z.name}}</span>
-                        </label>
-                      </p>
-                    </div>
-                    <div class="section">
-                      <span>Watering Days</span>
-                      <div>
-                        <a style="margin-right:10px; margin-top: 10px;" v-for="(day, idx) in days" :key="idx" v-bind:class="{ 'day-btn-enabled': s.days.includes(day) }" class="day-btn btn-flat" v-on:click="updateDay(s, day)">{{day}}</a>
-                      </div>
-                    </div>
-                    <div class="section">
-                      <span>Watering Start Time</span>
-                      <span class="right">
-                        <input type="text" class="timepicker" :id="'schedule_' +s.id+ '_time'" :value="renderTime(s.hour, s.minute)" v-on:change="dirty(s)">
-                        <i class="material-icons right green-text" style="margin-left: 0px; margin-right: -5px; color: color: #50AE54;">chevron_right</i>
-                      </span>
-                    </div>
-                    <div class="section">
-                      <span>Watering Duration</span>
-                      <span class="right" style="color: #50AE54; font-weight: bold;">{{s.duration}} minute(s)</span>
-                      <input type="range" id="duration-slider" min="1" max="60" v-model="s.duration" v-on:change="dirty(s)" />
+                  <div class="section">
+                    <span>Watering Days</span>
+                    <div>
+                      <a style="margin-right:10px; margin-top: 10px;" v-for="(day, idx) in days" :key="idx" v-bind:class="{ 'day-btn-enabled': s.days.includes(day) }" class="day-btn btn-flat" v-on:click="updateDay(s, day)">{{day}}</a>
                     </div>
                   </div>
                   <div class="section">
-                      <a href="#!" class="btn green right" style="right: 0" v-on:click="updateSchedule(s)"><i class="material-icons left">done</i>Save</a>
-                      <a href="#!" class="btn-flat red-text modal-trigger" v-on:click="deleteSchedule(s.id)"><i class="material-icons left" >delete</i>Delete</a>
-                    </div>
+                    <span>Watering Start Time</span>
+                    <span class="right">
+                      <input type="text" class="timepicker" :id="'schedule_' +s.id+ '_time'" :value="renderTime(s.hour, s.minute)" v-on:change="dirty(s)">
+                      <i class="material-icons right green-text" style="margin-left: 0px; margin-right: -5px; color: color: #50AE54;">chevron_right</i>
+                    </span>
+                  </div>
+                  <div class="section">
+                    <span>Watering Duration</span>
+                    <span class="right" style="color: #50AE54; font-weight: bold;">{{s.duration}} minute(s)</span>
+                    <input type="range" id="duration-slider" min="1" max="60" v-model="s.duration" v-on:change="dirty(s)" />
+                  </div>
                 </div>
-              </li>
-            </ul>
+                <div class="section">
+                  <a href="#!" class="btn green right" style="right: 0" v-on:click="updateSchedule(s)"><i class="material-icons left">done</i>Save</a>
+                  <a href="#!" class="btn-flat red-text modal-trigger" v-on:click="deleteSchedule(s.id)"><i class="material-icons left">delete</i>Delete</a>
+                </div>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
   </dashboard>
 </template>
-
 <script>
 /* global M */
 import Dashboard from '@/components/Dashboard'
@@ -150,11 +142,13 @@ import swal from 'sweetalert'
 
 export default {
   name: 'Home',
-  components: { Dashboard },
-  data () {
+  components: {
+    Dashboard
+  },
+  data() {
     return {
       api: {
-        basepath: 'http://localhost:5000/api/v1'
+        basepath: `http://${process.env.VUE_APP_API_HOSTNAME}:${process.env.VUE_APP_API_PORT}/api/v1`
       },
       meta: {
         time_now: moment().format('LT')
@@ -171,7 +165,7 @@ export default {
       }
     }
   },
-  mounted () {
+  mounted() {
     const vm = this
     vm.getData()
     setTimeout(function() {
@@ -181,14 +175,14 @@ export default {
   methods: {
 
     // HELPER METHODS
-    
+
     renderTime(h, m) {
       if (h < 10) {
-        h = String(0) + String (h)
+        h = String(0) + String(h)
       }
       return moment(String(h) + String(m), 'HHmm').format('LT')
     },
-    initM () {
+    initM() {
       var elems = document.querySelectorAll('.collapsible');
       M.Collapsible.init(elems);
 
@@ -207,10 +201,10 @@ export default {
 
     },
 
-    dirty (schedule) {
+    dirty(schedule) {
       schedule.dirty = true
     },
-    updateDay (zone, day) {
+    updateDay(zone, day) {
       if (zone.days.includes(day)) {
         zone.days.splice(zone.days.indexOf(day), 1)
       } else {
@@ -218,7 +212,7 @@ export default {
       }
       zone.dirty = true
     },
-    updateNewScheduleDay (day) {
+    updateNewScheduleDay(day) {
       if (this.new_schedule.days.includes(day)) {
         this.new_schedule.days.splice(this.new_schedule.days.indexOf(day), 1)
       } else {
@@ -248,12 +242,22 @@ export default {
 
     getData() {
       const vm = this
-      axios.get(vm.api.basepath + '/zones').then((res) => {
+      axios.get(vm.api.basepath + '/zones', {
+        auth: {
+          username: process.env.VUE_APP_API_USERNAME,
+          password: process.env.VUE_APP_API_PASSWORD
+        }
+      }).then((res) => {
         vm.zones = res.data
-        axios.get(vm.api.basepath + '/schedules').then((res) => {
+        axios.get(vm.api.basepath + '/schedules', {
+          auth: {
+            username: process.env.VUE_APP_API_USERNAME,
+            password: process.env.VUE_APP_API_PASSWORD
+          }
+        }).then((res) => {
           vm.schedules = res.data
           vm.initM()
-        }) 
+        })
       })
     },
 
@@ -262,7 +266,12 @@ export default {
     updateZone(z) {
       const vm = this
       z.status = z.status ? 1 : 0
-      axios.put(vm.api.basepath + '/zones/' + z.id, z).then((res) => {
+      axios.put(vm.api.basepath + '/zones/' + z.id, z, {
+        auth: {
+          username: process.env.VUE_APP_API_USERNAME,
+          password: process.env.VUE_APP_API_PASSWORD
+        }
+      }).then((res) => {
         console.log(res)
         vm.getData()
       }).catch((error) => {
@@ -298,7 +307,14 @@ export default {
           zones: vm.new_schedule.zones.map(x => parseInt(x.split('_').pop())),
           duration: vm.new_schedule.duration,
         }
-        axios.post(vm.api.basepath + '/schedules', schedule).then((res) => {
+        axios.post(vm.api.basepath + '/schedules',
+          schedule, {
+            auth: {
+              username: process.env.VUE_APP_API_USERNAME,
+              password: process.env.VUE_APP_API_PASSWORD
+            }
+          }
+        ).then((res) => {
           console.log(res)
           vm.getData()
         }).catch((error) => {
@@ -312,8 +328,8 @@ export default {
       const vm = this
       var zs = []
       for (var i = 0; i < vm.zones.length; i++) {
-        console.log($('#schedule_' + s.id +'_zone_' + vm.zones[i].id).is(':checked'))
-        if ($('#schedule_' + s.id +'_zone_' + vm.zones[i].id).is(':checked')){
+        console.log($('#schedule_' + s.id + '_zone_' + vm.zones[i].id).is(':checked'))
+        if ($('#schedule_' + s.id + '_zone_' + vm.zones[i].id).is(':checked')) {
           zs.push(vm.zones[i].id)
         }
       }
@@ -332,7 +348,12 @@ export default {
       if (errors.length > 0) {
         swal("Invalid inputs", String(errors).replace(/,/g, ''), "error");
       } else {
-        axios.put(vm.api.basepath + '/schedules/' + s.id, s).then((res) => {
+        axios.put(vm.api.basepath + '/schedules/' + s.id, s, {
+          auth: {
+            username: process.env.VUE_APP_API_USERNAME,
+            password: process.env.VUE_APP_API_PASSWORD
+          }
+        }).then((res) => {
           console.log(res)
           vm.getData()
         }).catch((error) => {
@@ -344,38 +365,43 @@ export default {
     deleteSchedule(id) {
       const vm = this
       swal({
-        title: "Are you sure?",
-        text: "Do you want to delete this schedule?",
-        icon: "warning",
-        buttons: true,
-        dangerMode: true,
-      })
-      .then((willDelete) => {
-        if (willDelete) {
-          axios.delete(vm.api.basepath + '/schedules/' + id).then((res) => {
-            console.log(res)
-            vm.getData()
-          }).catch((error) => {
-            console.log(error)
-            swal("Oops!", "Something went wrong. Please try again.", "error")  
-          })
-          swal("Schedule deleted", {
-            icon: "success",
-          });
-        }
-      });
+          title: "Are you sure?",
+          text: "Do you want to delete this schedule?",
+          icon: "warning",
+          buttons: true,
+          dangerMode: true,
+        })
+        .then((willDelete) => {
+          if (willDelete) {
+            axios.delete(vm.api.basepath + '/schedules/' + id, {
+              auth: {
+                username: process.env.VUE_APP_API_USERNAME,
+                password: process.env.VUE_APP_API_PASSWORD
+              }
+            }).then((res) => {
+              console.log(res)
+              vm.getData()
+            }).catch((error) => {
+              console.log(error)
+              swal("Oops!", "Something went wrong. Please try again.", "error")
+            })
+            swal("Schedule deleted", {
+              icon: "success",
+            });
+          }
+        });
     }
 
   }
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 @import '../assets/styles/_variables.scss';
 @import '../assets/styles/_overrides.scss';
 @import '../assets/styles/_forms.scss';
-.collapsible-body{
+
+.collapsible-body {
   background-color: $grey-lighter;
   padding: 0;
 
@@ -431,13 +457,14 @@ label {
   text-align: center !important;
 }
 
-[type="checkbox"].filled-in:checked + label:after {
+[type="checkbox"].filled-in:checked+label:after {
   border: 2px solid $green;
   background-color: $green;
 }
 
-[type="checkbox"]:checked + label:before {
+[type="checkbox"]:checked+label:before {
   border-right: 2px solid $green;
   border-bottom: 2px solid $green;
 }
+
 </style>
